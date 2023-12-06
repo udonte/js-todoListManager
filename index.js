d275f5a3821219274280b565e0ab60f1ca07860a
George Udonte <georgeudonte@gmail.com>
2023-12-06 19:18:02 +0100

    added template function to add

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const addForm = document.querySelector("form.add");
 const taskList = document.querySelector("ul.tasks");
+
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const taskValue = addForm.task.value.trim();
-  if (taskValue.length) {
-    taskList.innerHTML += `<li>
-    <span>${taskValue}</span
-    ><i class="bi bi-trash-fill delete"></i>
-  </li>`;
-    addForm.reset();
-  } else {
+  let value = addForm.task.value.trim();
+  if (value.length) {
+    taskList.innerHTML += createTemplate(value);
   }
+  addForm.reset();
 });
 
-console.log(taskList.innerHTML);
+const createTemplate = (value) => {
+  return `
+      <li>
+        <span>${value}</span>
+        <i class="bi bi-trash-fill delete"></i>
+      </li>`;
+};
