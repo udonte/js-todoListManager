77656cf9ebd45eaa635945350258c9846ca6a81f
George Udonte <georgeudonte@gmail.com>
2023-12-07 08:43:50 +0100

    added delete and clear all functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const addForm = document.querySelector("form.add");
 const taskList = document.querySelector("ul.tasks");
+const clearAll = document.querySelector(".clear");
 
+//add task
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let value = addForm.task.value.trim();
@@ -17,3 +19,18 @@ const createTemplate = (value) => {
         <i class="bi bi-trash-fill delete"></i>
       </li>`;
 };
+
+//delete task
+taskList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+// clear all
+clearAll.addEventListener("click", (e) => {
+  const taskItems = document.querySelectorAll("li");
+  taskItems.forEach((item) => {
+    item.remove();
+  });
+});
